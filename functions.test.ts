0227ba369983dbf0ee448bcb8aa328ecd7b6fa8e
Steven Epstein <sbepstein42@gmail.com>
2021-11-20 17:46:19 -0500

    Part 3: Unit Tests completed

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,15 @@
 const {shuffleArray} = require('./utils')
+const {bots} = require("./data.js")
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+    /* the function and instructions are labeled incorrectly. (typeof bots) returns "object". This is because data.js contains the objects bots, which is not an array. So the function shuffleArray from ./utils.js is actually shuffling an object */
+    test("return an object", () => {
+        expect(typeof shuffleArray(bots)).toBe("object")
+    })
+    test("--with a parameter of bots--have a length equal to bots.length", () => {
+        expect(shuffleArray(bots).length).toEqual(bots.length)
+    })
+    test("return a shuffled array that does not match the original array", () => {for(let i=0; i<25; i++){
+        expect(shuffleArray(bots)).not.toBe(bots)
+    }})
 })
